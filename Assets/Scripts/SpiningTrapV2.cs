cf9db4b51ca601d0c0a89752169ff6cb18236455
Michal <zienowit@gmail.com>
2021-06-06 17:13:12 +0200

    pulapki Lukiego

---
+++ Assets/Scripts/SpiningTrapV2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiningTrapV2 : MonoBehaviour
+{
+    public float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, Time.deltaTime * speed, Space.Self);
+    }
+}
